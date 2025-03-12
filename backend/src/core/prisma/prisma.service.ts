3a8a0af9c8eadb897f16e82e1a1d931cbaf2ef28
k1ego <ladonin05@mail.ru>
2025-03-13 01:47:33 +0300

    Окончательная настройка Prisma

---
+++ backend/src/core/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+// управления подключением к базе данных с помощью Prisma 
+
+import { Injectable, type OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/generated'
+
+@Injectable()
+export class PrismaService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
+	// при инициализации модуля будет устанавливаться соеденинение с бд
+	public async onModuleInit() {
+		await this.$connect()
+	}
+
+	// при разрешении модуля, здесь разрывается связь с бд
+	public async onModuleDestroy() {
+		await this.$disconnect()
+	}
+}
